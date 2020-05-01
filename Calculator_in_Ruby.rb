5c346c7705a42036cd801b98b724aea351f13e8f
Samyak Bambole <64409597+samyakbambole@users.noreply.github.com>
2020-05-01 09:55:32 +0530

    I added multiplication
    I made the calculator more usable by adding multiplication to it.

--- Calculator_in_Ruby.rb
+++ Calculator_in_Ruby.rb
@@ -1,6 +1,7 @@
 puts "Enter a Number: "
-num1 = gets.chomp() # Gets will get the user input and chomp will get rid of the new line.
+num1 = gets.chomp().to_f # Gets will get the user input and chomp will get rid of the new line.
 puts "Enter another Number: "
-num2 = gets.chomp()
+num2 = gets.chomp().to_f # to_f will convert the variables into decimals
 
-puts (num1.to_f + num2.to_f) # to_f will convert the variables into decimals
+puts (num1 + num2) 
+puts (num1 * num2)
